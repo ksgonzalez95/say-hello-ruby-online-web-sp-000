14a9181d8ae041701d5a5b4a567c3d86c36bc33b
Kimberly Gonzalez <ksgonzalez95@gmail.com>
2021-05-25 15:50:11 +0000

    Automatically backed up by Learn

--- spec/say_hello.rb
+++ spec/say_hello.rb
@@ -3,8 +3,8 @@ require_relative '../say_hello'
 RSpec.configure do |config|
   # config here
 
-def say_hello (name = "Ruby Programming")
+def say_hello (name = "Kent Back")
   "Hello, #{name}!"
 end
 
-say_hello ("Gabriela")
+say_hello ("Kent Back")
